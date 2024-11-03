fbf593b2e1b19abbfc8e12bb0a8f8cd3fc867f51
Bikash Kakati <bikashkakati796@gmail.com>
2024-11-03 20:30:22 +0530

    Add: Toast container

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import SignIn from "./pages/auth/sign-in/SignIn";
 import RootLayout from "./pages/root/RootLayout";
-import MainDashboardPage from "./pages/root/main-dashboard/MainDashboardPage";
 import DoctorsPage from "./pages/root/doctors/DoctorsPage";
+import MainDashboardPage from "./pages/root/main-dashboard/MainDashboardPage";
 import PatientsPage from "./pages/root/patients/PatientsPage";
 import ServicesPage from "./pages/root/services/ServicesPage";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 const App: React.FC = () => {
   return (
+    <>
+    <ToastContainer position="bottom-right" hideProgressBar={false} />
     <Routes>
       {/* AUTH Section*/}
       <Route path="/sign-in" element={<SignIn />} />
@@ -20,6 +24,7 @@ const App: React.FC = () => {
         <Route path="/services" element={<ServicesPage />} />
       </Route>
     </Routes>
+    </>
   );
 };
 
